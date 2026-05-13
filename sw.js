b09904862d7aeb577e43e37ef8cbcebff5a33a24
ahmadaljazee <ahmad.aljazee@gmail.com>
2026-05-13 07:57:08 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Nesma Service Worker Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // هذا الجزء يسمح للتطبيق بطلب البيانات من السيرفر (رندر)
-  e.respondWith(fetch(e.request));
-});
